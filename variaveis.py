46cf4a1071372b89302216663e0510cbdee3a396
mathiassammer <mathias.sammer.946@gmail.com>
2022-07-09 14:29:24 -0300

    Variáveis no python

---
+++ variaveis.py
@@ -0,0 +1,32 @@
+#Usando snake_case para nomes de variaveis
+idade_mathias = 24
+print(idade_mathias) #imprime a idade_mathias
+
+#Quais tipos de dados podemos armazenar na memória do computador?
+#Números
+ano = 2022
+#Valores booleanos
+esta_aberto = True
+#Strings
+nome = "Mathias Sammer"
+
+#Podemos agrupar variaveis:
+a,b,c,d = 1,2,3,4
+print(a)
+
+#Como saber o tipo da variável?
+type(a) #<class int>, ou seja, do tipo inteiro
+
+#-----------------------------------------------------------
+
+#DESAFIO
+# Descubra qual é o tipo de cada um desses dados:
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = 'Bom dia'
+variavel_4 = 15
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
