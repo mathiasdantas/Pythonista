54d7322b91bf72cc890b02030476dbe414cfd4d5
mathiassammer <mathias.sammer.946@gmail.com>
2022-07-11 19:08:34 -0300

    numbers and operations maths no python

---
+++ number_operations_maths.py
@@ -0,0 +1,32 @@
+# TIPOS DE NÚMEROS QUE PODEM SER USADOS NO PYTHON
+from math import ceil
+
+
+a = 20
+b = 20.5
+print(type(a))
+print(type(b))
+
+# Operações Matemáticas
+print(10 + 6)  # Adição
+print(10 - 6)  # Subtração
+print(10 * 6)  # Multiplicação
+print(10 / 6)  # Divisão
+print(10 // 6)  # divisão de inteiro
+print(10 % 6)  # Resto da divisão // modulos
+print(10 ** 6)  # exponenciais
+
+# Atalho para atribuição mais rapida
+a = 10
+a = a + 5
+a += 5  # Mais rapido
+
+b = 20
+b = b - 2
+b -= 2  # Mais rapido
+
+# Operações matemáticas comuns
+print(round(5.7))  # Arredondar um valor
+print(ceil(2.2))  # Forçar o arredondamento para o número mais alto
+
+# PARA MAIS OPERAÇÕES, CONSULTAR A DOCUMENTAÇÃO!
