b62c4659c6e761d6bd0510a9ce7f02e3b438237b
mathiassammer <mathias.sammer.946@gmail.com>
2022-07-11 18:53:02 -0300

    input no python

---
+++ input.py
@@ -0,0 +1,7 @@
+# RECEBENDO DADOS DO USUÁRIO
+senha = input("Digite sua senha: ")
+print(senha)
+print(type(senha))  # tipo de saída
+
+quantidade_de_filmes = input("Quantos filmes você vai assistiu?")
+print(type(quantidade_de_filmes))
